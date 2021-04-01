e03e4f742e29655ca74d180a15ea2680708bef76
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-03-31 18:22:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(arrayOfObj) {
+    const win = arrayOfObj.find( function(obj) {
+        if (obj.result === "W") { 
+            return obj
+        }
+    })
+    if (win) {
+        return win.year
+    }
+}
